7647957e3a3b62005400c6193c2aaf52c47c5f69
Jakub Nowakowski <101364218+JakubNowakowski98@users.noreply.github.com>
2022-11-30 19:32:22 +0100

    Final branch - all merged into this one - ready to use - by Jakub Nowakowski, Jakub Kleszko and Michał Nurzyński

---
+++ user/target/site/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"pwo.user","c":"User","l":"actionPerformed(ActionEvent)","u":"actionPerformed(java.awt.event.ActionEvent)"},{"p":"pwo.user","c":"User","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"pwo.user","c":"User","l":"User()","u":"%3Cinit%3E()"}];updateSearchResults();
